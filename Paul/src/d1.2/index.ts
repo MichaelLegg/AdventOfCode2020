0b9c0395a18567e9032a90c29fda501911ddbd98
BOTPaul <3285385+BOTPaul@users.noreply.github.com>
2020-12-01 21:32:30 +0000

    Better optimisation for 1.2

--- Paul/src/d1.2/index.ts
+++ Paul/src/d1.2/index.ts
@@ -1,8 +1,7 @@
 import data from "./data";
 
 const start = process.hrtime();
-const filtered = data.filter(d => d < 1200); // this is maybe very cheaty ;)
-console.table(filtered.map(a => filtered.map(b => filtered.map(c => ({sum: a + b + c, mul: a * b * c })).filter(c => c.sum === 2020).map(c => c.mul)).filter(b => b.length > 0)).filter(a => a.length > 0));
+console.table(data.map(a => data.map(b => data.map(c => ({sum: a + b + c, mul: a * b * c })).filter(c => c.sum === 2020).map(c => c.mul)).filter(b => b.length > 0)).filter(a => a.length > 0));
 const end = process.hrtime(start);
 
  console.info('Execution time: %ds %dms', end[0], end[1] / 1000000);
---
+++ Paul/src/d1.2v2/index.ts
@@ -0,0 +1,9 @@
+import data from "./data";
+
+const start = process.hrtime();
+const min = data.reduce((a, b) => a < b ? a : b) * 3.5;
+const filtered = data.filter(d => d <= min);
+console.table(filtered.map(a => filtered.map(b => filtered.map(c => ({ sum: a + b + c, mul: a * b * c })).filter(c => c.sum === 2020).map(c => c.mul)).filter(b => b.length > 0)).filter(a => a.length > 0));
+const end = process.hrtime(start);
+
+ console.info('Execution time: %ds %dms', end[0], end[1] / 1000000);

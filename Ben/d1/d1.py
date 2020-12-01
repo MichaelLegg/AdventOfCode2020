7d9d98865713aa2b9638fb6d7e112d41a12afb68
Ben Lancaster <bdl@live.co.uk>
2020-12-01 21:07:50 +0000

    d1: p2 faster with hashmap

--- Ben/d1/d1.py
+++ Ben/d1/d1.py
@@ -8,7 +8,7 @@ def timer(func):
     val = func(*args, **kwargs)
     te = time.perf_counter()
     rt = te - ts
-    print(f"Finished {func.__name__} in {rt:.8f}s")
+    print(f"Finished {func.__name__:<10} {rt:.8f}s = {val}")
     return val
   return wrapper
 
@@ -35,9 +35,29 @@ def d1p2(nums):
           #print("Found! mul =", n*m*p)
           return n*m*p
 
+@timer
+def d1p2h(nums):
+  hm = {}
+  for n in list(nums):
+    for m in list(nums):
+      key = (n,m)
+      if n == m: 
+        continue
+      if key not in hm: 
+        hm[key] = n+m
+
+  i = 0
+  for k,v in hm.items():
+    n = nums[i%len(nums)]
+    if (v+n) == 2020:
+      return k[0]*k[1]*n
+    i += 1
+
+
 with open("input.txt", "r") as f:
   # convert input to list of ints
   nums = list(map(int, f.readlines()))
   # Run challenges
-  print(d1p1(nums))
-  print(d1p2(nums))
+  d1p1(nums)
+  d1p2(nums)
+  d1p2h(nums)

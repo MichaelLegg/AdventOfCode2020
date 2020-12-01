482d30a13d60b9aba64d91372e3e9cc06356550f
Kane Small <kanesmall@outlook.com>
2020-12-01 22:17:02 +0000

    - Day 1, parts 1 & 2. Many loops...

---
+++ Kane/src/day_1/part_1/index.ts
@@ -0,0 +1,9 @@
+import data from "../data";
+
+for(let i = 0; i < data.length; i++) {
+    for(let j = 0; j < data.length; j++) {
+        if(data[i] + data[j] === 2020) {
+            console.log(data[i] * data[j]);
+        }
+    }
+}
---
+++ Kane/src/day_1/part_2/index.ts
@@ -0,0 +1,11 @@
+import data from "../data";
+
+for(let i = 0; i < data.length; i++) {
+    for(let j = 0; j < data.length; j++) {
+        for(let k = 0; k < data.length; k++) {
+            if(data[i] + data[j] + data[k] === 2020) {
+                console.log(data[i] * data[j] * data[k]);
+            }
+        }
+    }
+}

ad57b984c2f141c690edc397d6dbb2c926ac0e3b
Kane Small <kanesmall@outlook.com>
2020-12-02 07:21:06 +0000

    - Stole Paul's timing code. - Wrapped my code in a function and returned once value had been found, making the execution time much quicker.

--- Kane/src/day_1/part_1/index.ts
+++ Kane/src/day_1/part_1/index.ts
@@ -1,9 +1,18 @@
 import data from "../data";
 
-for(let i = 0; i < data.length; i++) {
-    for(let j = 0; j < data.length; j++) {
-        if(data[i] + data[j] === 2020) {
-            console.log(data[i] * data[j]);
+const start = process.hrtime();
+
+function compare() {
+    for(let i = 0; i < data.length; i++) {
+        for(let j = 0; j < data.length; j++) {
+            if(data[i] + data[j] === 2020) {
+                return data[i] * data[j];
+            }
         }
     }
 }
+console.log(compare());
+
+const end = process.hrtime(start);
+
+console.info('Execution time: %ds %dms', end[0], end[1] / 1000000);

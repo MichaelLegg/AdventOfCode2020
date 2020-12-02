64a57e44d0205fc608725cc271c0f09ef9963ab6
Kane Small <kanesmall@outlook.com>
2020-12-02 07:25:34 +0000

    Did the same for part 2, wrapped in a function.

--- Kane/src/day_1/part_2/index.ts
+++ Kane/src/day_1/part_2/index.ts
@@ -1,11 +1,21 @@
 import data from "../data";
 
-for(let i = 0; i < data.length; i++) {
-    for(let j = 0; j < data.length; j++) {
-        for(let k = 0; k < data.length; k++) {
-            if(data[i] + data[j] + data[k] === 2020) {
-                console.log(data[i] * data[j] * data[k]);
+const start = process.hrtime();
+
+function compare() {
+    for(let i = 0; i < data.length; i++) {
+        for(let j = 0; j < data.length; j++) {
+            for(let k = 0; k < data.length; k++) {
+                if(data[i] + data[j] + data[k] === 2020) {
+                    console.log(data[i] * data[j] * data[k]);
+                }
             }
         }
     }
 }
+
+console.log(compare());
+
+const end = process.hrtime(start);
+
+console.info('Execution time: %ds %dms', end[0], end[1] / 1000000);

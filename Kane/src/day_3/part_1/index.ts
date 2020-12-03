239b6a3b67bb64abeb6d3430c4c64f41baeb3990
Kane Small <kanesmall@outlook.com>
2020-12-03 22:28:32 +0000

    I think I'm close to getting day 3, part 1...

--- Kane/src/day_3/part_1/index.ts
+++ Kane/src/day_3/part_1/index.ts
@@ -3,9 +3,23 @@ import data from "../data";
 const start = process.hrtime();
 
 function pwdPolicyCheck(input: string[]) {
-    
+    let count = 4;
+    let treeCount = 0;
+    for(let x=1; x < input.length; x++) {
+        if(count > input[x].length) {
+            let original = input[x];
+            let i = input[x].length;
+            do {
+                input[x] += original;
+                i += input[x].length;
+            } while(count > i);
+            if(input[x].charAt(count) === "#") treeCount++;
+        } else if((count <= input[x].length) && input[x].charAt(count) === "#") treeCount++;
+        count += 3;
+    }
+    return treeCount;
 }
-console.log(pwdPolicyCheck(data));
+console.log("Puzzle answer: " + pwdPolicyCheck(data));
 
 const end = process.hrtime(start);
 console.info('Execution time: %ds %dms', end[0], end[1] / 1000000);

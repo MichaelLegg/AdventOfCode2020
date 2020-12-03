0dae288f7fc827f62c2927abd4c8803994eb4f9a
Michael Legg <michael.legg@outlook.com>
2020-12-03 23:19:02 +0000

    D3 P2

--- Mikey/src/3/s2.ts
+++ Mikey/src/3/s2.ts
@@ -1,15 +1,32 @@
 import Fs from "fs"
 
-export default function Solution(): number{
-    let numMatches = 0;
-    Fs.readFileSync(`src/2/data.txt`).toString().split('\r\n').filter(x => x.length > 0).forEach(row =>
-    {
-        row  = row.replace("-", " ");
-        row = row.replace(":", "");
-        const pwArray: string[]  = row.split(" ");     
+interface rule {
+    right: number;
+    down: number;
+}
 
-        if(+(pwArray[3][+pwArray[0]-1] === pwArray[2]) ^ +(pwArray[3][+pwArray[1]-1] === pwArray[2]))
-            numMatches++;                         
+export default function Solution(): number{
+    let input: string[] = Fs.readFileSync(`src/3/data.txt`).toString().split('\r\n').filter(x => x.length > 0);
+
+    let rules: rule[] = [{right: 1, down: 1}, {right:3, down:1}, {right: 5, down:1}, {right: 7, down: 1}, { right: 1, down: 2}]
+  
+    let rowLength = input[0].length;
+    let total = 1;
+    rules.forEach(rule => {
+        let numMatches = 0;
+        let yCounter = 0;
+
+        for(let i = 0; i < input.length; i+=rule.down){
+            if(input[i][yCounter] === '#') 
+                numMatches++
+    
+            yCounter+=rule.right;
+            const remainder = yCounter - rowLength;
+            if(remainder >= 0)
+                yCounter = remainder        
+        }  
+        total*=numMatches;
     });
-    return numMatches;
+       
+    return total;
 }

57cc1b0f5f8726b7bd615287eb1454946c4b16cb
Ben Lancaster <bdl@live.co.uk>
2020-12-05 19:28:09 +0000

    d5: p2: python

--- Ben/d5/d5.py
+++ Ben/d5/d5.py
@@ -29,12 +29,37 @@ def d5p1(data):
     col = int(line[7:11],2)
     # calculate seat id
     id = row*8+col
+    print(id)
     if id > largest:
       largest = id
   return largest
 
+@timer
+def d5p2(data):
+  data = data.split("\n")
+  seats = []
+  for line in data:
+    # convert to binary
+    line = line.replace("F", "0")
+    line = line.replace("B", "1")
+    line = line.replace("L", "0")
+    line = line.replace("R", "1")
+    # extract row and columns
+    row = int(line[:7],2)
+    col = int(line[7:11],2)
+    # calculate seat id
+    id = row*8+col
+    seats.append(id)
+  # find missing seat
+  # missing seat is the missing consecutive number
+  seats = sorted(seats)
+  for i in range(len(seats)):
+    id = seats[i]
+    if seats[i+1] != id+1:
+      return id+1
 
 with open("input.txt", "r") as f:
   data = f.read()
   # Run challenges
   d5p1(data)
+  d5p2(data)

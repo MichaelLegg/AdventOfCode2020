0c75c5b1714fae4580a4c3b16b774d97f041e01b
Michael Legg <michael.legg@outlook.com>
2020-12-05 19:11:03 +0000

    D5 P1

---
+++ Mikey/src/5/s1.ts
@@ -0,0 +1,22 @@
+import Fs from "fs"
+
+export default function Solution(): number{
+    let input: string[] = Fs.readFileSync(`src/5/data.txt`).toString().split('\r\n').filter(x => x.length > 0);    
+    const seatIds: number[] = [];
+
+    input.forEach(currentRow => {
+        const row = findCenter("F", 0, 127, currentRow.slice(0, 7))
+        const column = findCenter("L", 0, 7, currentRow.slice(7, currentRow.length));
+        seatIds.push((row * 8) + column);
+    });
+    return seatIds.sort((a, b) => b-a)[0];
+}   
+
+function findCenter(lowerChar: string, lowerBound: number, upperBound: number, characters: string): number{
+    for(let i = 0; i < characters.length-1; i++){
+        const newVal = (lowerBound + upperBound) / 2;
+        if(characters[i] === lowerChar) upperBound =  Math.floor(newVal);
+        else lowerBound = Math.ceil(newVal);
+    }
+    return characters[characters.length-1] === lowerChar ? lowerBound : upperBound;
+}

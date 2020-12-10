6d83cd0787c2639d12d379f2cee2d920864af479
Ben Lancaster <bdl@live.co.uk>
2020-12-10 23:02:52 +0000

    d10: p2: python recursive + memo

--- Ben/d10/d10.py
+++ Ben/d10/d10.py
@@ -34,7 +34,56 @@ def d10p1(data):
   diff_counters[3] += 1
   return diff_counters[1]*diff_counters[3]
 
+memory = {}
+
+import copy
+def search(current, chargers, i, ls):
+  # ls = copy.deepcopy(ls)
+  # ls.append(current)
+  # find next possible chargers in the bag
+  next3 = chargers[i:i+3]
+  possible = tuple(filter(lambda x: x <= current+3, next3))
+
+  # store solution to avoid redoing
+  if possible in memory:
+    return memory[possible]
+
+  # if no possible next charger, we've reached the end
+  if len(possible) == 0:
+    return 1
+  # for each possible charger, start a new search tree using it
+  s = 0
+  for pi in range(len(possible)):
+    charger = possible[pi]
+    s += search(charger, chargers, i+pi+1, ls)
+
+  memory[possible] = s
+  return s
+
+
+@timer
+def d10p2(data):
+  chargers = map(int, data.split("\n"))
+  chargers = sorted(chargers)
+  result = []
+  return search(0, chargers, 0, [])
+
+def search_tr(current, chargers, i, ls):
+  return 0
+
+def go(i, current, acc, data):
+  next3 = data[i:i+3]
+  possible = list(filter(lambda x: x <= current+3, next3))
+  if len(possible) == 0:
+    return 1
+
+  sum = 0
+  for pi in range(len(possible)):
+    c = possible[i]
+    go(pi, c, acc, data)
+
 with open("input.txt", "r") as f:
   data = f.read()
   # Run challenges
   d10p1(data)
+  d10p2(data)

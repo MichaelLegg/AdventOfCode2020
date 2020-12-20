c029398d2c1e3efc37821d36bc78ffef3009c7ea
Ben Lancaster <bdl@live.co.uk>
2020-12-20 20:55:46 +0000

    d15: p2: python faster with hashmap

--- Ben/d15/d15.py
+++ Ben/d15/d15.py
@@ -15,16 +15,34 @@ def timer(func):
   return wrapper
 
 def nth_number(data, N):
-  data = list(reversed(list(map(int, data.split(",")))))
-  for i in range(len(data),N):
-    last_number = data[0]
-    try:
-      last_occurence = data[1:].index(last_number) +1
-    except:
-      last_occurence = 0
+  data = list(map(int, data.split(",")))
+  # spoken_number: turn_it_was_last_spoken 
+  hm_lastspoken = {}
 
-    data.insert(0, last_occurence)
-  return data[0]
+  # prefill last spoken tracker with starting data
+  for i in range(len(data)-1):
+    hm_lastspoken[data[i]] = i
+
+  # start on the last starting number
+  last_number = data[-1]
+  turn = len(data)-1
+
+  # iterate over future turns
+  for i in range(N-len(data)):
+    # if already spoken this number, find the difference
+    # between current turn and the turn it was last spoken
+    if last_number in hm_lastspoken:
+      nxt_num = turn - hm_lastspoken[last_number]
+    else:
+      # have not spoken yet, so next number is 0
+      nxt_num = 0
+
+    # record that we've spoken this number on this turn
+    hm_lastspoken[last_number] = turn
+    # move to next number
+    last_number = nxt_num
+    turn += 1
+  return last_number
 
 @timer
 def d15p1(data):

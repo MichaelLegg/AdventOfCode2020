4ab45df4e122b0bd84b0d943e1b61d85436f6be8
Ben <bdl@live.co.uk>
2020-12-22 20:11:36 +0000

    d16: p2: python

--- Ben/d16/d16.py
+++ Ben/d16/d16.py
@@ -62,12 +62,58 @@ def d16p1(data):
             # print(num, "in", valid_nums)
         if not valid:
           invalid_nums.append(num)
-          print(num, "not valid")
+          # print(num, "not valid")
 
   return sum(invalid_nums)
 
+def get_range(line):
+  m = re.match("(\w+[ ]*[\w+]*): (\d+)-(\d+) or (\d+)-(\d+)", line)
+  if not m:
+    return None
+  line_type = m.group(1)
+  r1 = (int(m.group(2)), int(m.group(3))+1)
+  r2 = (int(m.group(4)), int(m.group(5))+1)
+  r1 = range(r1[0], r1[1])
+  r2 = range(r2[0], r2[1])
+  return (line_type,r1,r2, [], [])
+
+
 @timer
 def d16p2(data):
+  data = data.split("\n")
+  nearby = False
+  rules = []
+  invalid_nums = []
+
+  for line in data:
+    
+    line_data = get_range(line)
+    if line_data:
+      rules.append(line_data)
+    
+    if line[:6] == "nearby":
+      nearby = True
+      continue
+      
+    if nearby:
+      vals = list(map(int, line.split(',')))
+      for col,num in enumerate(vals):
+        for (rule_type, r1, r2, possible, impossible) in rules:
+          if num in r1 or num in r2:
+            # number valid for rule
+            if col not in impossible and col not in possible:
+              possible.append(col)
+          else:
+            # not valid for rule
+            if col in possible:
+              possible.remove(col)
+            if col not in impossible:
+              impossible.append(col)
+            continue
+
+  for (rule_type, r1, r2, possible, impossible) in rules:
+    print(rule_type, possible)
+
   return 0
 
 with open("input.txt", "r") as f:

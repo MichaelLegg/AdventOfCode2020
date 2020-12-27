464c8d6d6a2ceaca9a7c3ef7c8256cd2637ee098
Ben <bdl@live.co.uk>
2020-12-27 00:56:20 +0000

    d19: p2: python broken

--- Ben/d19/d19.py
+++ Ben/d19/d19.py
@@ -2,11 +2,15 @@
 def solve(line):
     # print("top string=", line)
     passfail,remaining = match_or(new_rules[0], line)
-    if passfail and not remaining:
-        print('pass', line)
     # remaining strings are fails
     return passfail and not remaining
 
+# 0: 1
+# 1: 2 4
+# 2: 42 or 42 and 3
+# 3: a
+# 4: b
+#42: c
 def match_or(rule, line):
     pos = line
     rule_pass = False
@@ -32,11 +36,16 @@ def match_sequence(rule, line):
         # Match terminal symbol
         else:
             # terminal letter, so check it matches line[0]
+            if not line:
+                r_pass = False
+                break
+
             r_pass = line[0] == rr
             if not r_pass:
                 break
             line = line[1:]
     return r_pass,line
+
 
 new_rules = {}
 with open("input.txt", "r") as f:
@@ -47,7 +56,6 @@ with open("input.txt", "r") as f:
     # convert rule numbers into rule indexes
     for (idx,rhs) in rules:
         line_rule = []
-        print((idx,rhs))
         rhs = rhs.split(' | ')
         if rhs[0][0] in set('1234567890'):
             # non-terminal rule reference
@@ -64,3 +72,9 @@ with open("input.txt", "r") as f:
     p1 = sum([solve(line) for line in data])
     print(p1)
 
+    # 8: 42 | 42 8
+    # 11: 42 31 | 42 11 31
+    new_rules[8]  = [[42],[42,8]]
+    new_rules[11] = [[42,31],[42,11,31]]
+    p2 = sum([solve(line) for line in data])
+    print(p2)
